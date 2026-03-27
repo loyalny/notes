eb2a87a58988f966d0163933722bcc58285fa4f2
loyalny <notes@loyalny>
2026-03-27 17:21:34 +0300

    Первый коммит

---
+++ bin/installer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pip3 install -r ../requirements.txt
+
+pyinstaller --noconsole --onefile --name="Notes" --icon=../src/design/icons/icon.ico ../src/main.py
+
+chmod +x installer.sh
+
+read
